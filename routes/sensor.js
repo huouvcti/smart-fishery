e9629c24a8a55774e4c2f83736cc5e22fdb85294
huouvcti <huouvcti@gmail.com>
2022-07-13 23:01:13 +0900

    sensor 수정

--- routes/sensor.js
+++ routes/sensor.js
@@ -10,18 +10,5 @@ router.get("/", sensorCtrl.main);
 
 router.get("/send", sensorCtrl.send);
 
-// 수온 water temperature
-router.get("/wt", sensorCtrl.WT);
-
-// 용존 산소량 DO
-router.get("/do", sensorCtrl.DO);
-
-// 산성도 pH
-router.get("/ph", sensorCtrl.PH);
-
-
-// 염도 salinity
-router.get("/sa", sensorCtrl.SA);
-
 
 module.exports = router;
