c6cc4fcaacad02531363d753d99c7df67a3c7bb6
huouvcti <huouvcti@gmail.com>
2022-07-15 13:16:53 +0900

    last value

--- controller/sensorCtrl.js
+++ controller/sensorCtrl.js
@@ -4,6 +4,8 @@ const { paging } = require('./tool/paging');
 
 const sensorDAO = require('../model/sensorDAO');
 
+const location_login = "<script>alert(`로그인페이지로 이동`); location.href='/login';</script>";
+
 const main = async (req, res) => {
    
     if(req.session.user_key){
@@ -14,7 +16,7 @@ const main = async (req, res) => {
         
         res.render('../views/sensor', {parameters});
     } else{
-        res.send("<script>alert(`로그인페이지로 이동`); location.href='/login';</script>");
+        res.send(location_login);
     }
 }
 
@@ -26,7 +28,7 @@ const send = async (req, res) => {
         }
         res.render('../views/sensorSend', {parameters})
     } else{
-        res.send("<script>alert(`로그인페이지로 이동`); location.href='/login';</script>");
+        res.send(location_login);
     }
 }
 
@@ -38,15 +40,19 @@ const log = async (req, res) => {
         const pageSize = 20;
         const page = paging(currentPage, pageSize);
 
+        const user_key = req.session.user_key;
+
         const parameters = {
-            user_key: req.session.user_key,
+            user_key: user_key,
             offset: page.offset,
             limit: page.limit,
 
             // 검색
-            start_date: (req.query.start == undefined) ? "1970-01-01" : req.query.start,
-            end_date: (req.query.end == undefined) ? "3000-01-01" : req.query.end
+            start_date: (req.query.start == undefined || req.query.start == "") ? "1970-01-01" : req.query.start,
+            end_date: (req.query.end == undefined || req.query.end == "") ? "3000-01-01" : req.query.end
         }
+
+
         currentPage = page.currentPage;
 
         const pageCnt = await sensorDAO.log_page_cnt(parameters);
@@ -56,9 +62,9 @@ const log = async (req, res) => {
         
         console.log(cnt);
 
-        res.render('../views/log', {db_data, currentPage, cnt})
+        res.render('../views/log', {user_key, db_data, currentPage, cnt})
     } else{
-        res.send("<script>alert(`로그인페이지로 이동`); location.href='/login';</script>");
+        res.send(location_login);
     }
 }
 
